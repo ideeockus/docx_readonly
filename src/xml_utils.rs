e2e482153576793e8d235e1a1bfc9f83a53ebf81
radmirkus <radmirka745@gmail.com>
2023-03-13 12:59:57 +0300

    Fixed xml_utils

--- src/xml_utils.rs
+++ src/xml_utils.rs
@@ -9,49 +9,26 @@ pub fn apply_settings_readonly(settings_xml_bytes: &[u8]) -> Result<Vec<u8>, qui
 
     loop {
         match reader.read_event() {
-            // Ok(Event::Start(e)) if e.name().as_ref() == b"this_tag" => {
-            //
-            //     // crates a new element ... alternatively we could reuse `e` by calling
-            //     // `e.into_owned()`
-            //     let mut elem = BytesStart::new("my_elem");
-            //
-            //     // collect existing attributes
-            //     elem.extend_attributes(e.attributes().map(|attr| attr.unwrap()));
-            //
-            //     // copy existing attributes, adds a new my-key="some value" attribute
-            //     elem.push_attribute(("my-key", "some value"));
-            //
-            //     // writes the event to the writer
-            //     assert!(writer.write_event(Event::Start(elem)).is_ok());
-            // },
-            // Ok(Event::End(e)) if e.name().as_ref() == b"this_tag" => {
-            //     assert!(writer.write_event(Event::End(BytesEnd::new("my_elem"))).is_ok());
-            // },
             Ok(Event::Empty(e)) if e.name().as_ref() == b"w:documentProtection" => {
-            // Ok(e) if e.name().as_ref() == b"w:documentProtection" => {
                 println!("Found protection tag !!!");
+            }
+
+            Ok(Event::Start(e)) if e.name().as_ref() == b"w:settings" => {
                 let mut elem = e.into_owned();
-                elem.clear_attributes();
-                // <w:documentProtection
-                //     w:edit="readOnly"
-                //     w:enforcement="1"
-                //     w:cryptProviderType="rsaFull"
-                //     w:cryptAlgorithmClass="hash"
-                //     w:cryptAlgorithmType="typeAny"
-                //     w:cryptAlgorithmSid="4"
-                //     w:cryptSpinCount="0"
-                //     w:hash="i1Ge4q/1mFrm4dpp3YwuD26Jte4="
-                //     w:salt="ql69CaZ9XBh8gn1wvmwi+Q=="/>
-                elem.push_attribute(("w:edit", "readOnly"));
-                elem.push_attribute(("w:enforcement", "1"));
-                elem.push_attribute(("w:cryptProviderType", "rsaFull"));
-                elem.push_attribute(("w:cryptAlgorithmClass", "hash"));
-                elem.push_attribute(("w:cryptAlgorithmType", "typeAny"));
-                elem.push_attribute(("w:cryptAlgorithmSid", "4"));
-                elem.push_attribute(("w:cryptSpinCount", "0"));
-                elem.push_attribute(("w:hash", "i1Ge4q/1mFrm4dpp3YwuD26Jte4="));
-                elem.push_attribute(("w:salt", "ql69CaZ9XBh8gn1wvmwi+Q=="));
-                writer.write_event(Event::Empty(elem))?;
+                writer.write_event(Event::Start(elem))?;
+
+                let mut protection_elem = BytesStart::new("w:documentProtection");
+                protection_elem.clear_attributes();
+                protection_elem.push_attribute(("w:edit", "readOnly"));
+                protection_elem.push_attribute(("w:enforcement", "1"));
+                protection_elem.push_attribute(("w:cryptProviderType", "rsaFull"));
+                protection_elem.push_attribute(("w:cryptAlgorithmClass", "hash"));
+                protection_elem.push_attribute(("w:cryptAlgorithmType", "typeAny"));
+                protection_elem.push_attribute(("w:cryptAlgorithmSid", "4"));
+                protection_elem.push_attribute(("w:cryptSpinCount", "0"));
+                protection_elem.push_attribute(("w:hash", "i1Ge4q/1mFrm4dpp3YwuD26Jte4="));
+                protection_elem.push_attribute(("w:salt", "ql69CaZ9XBh8gn1wvmwi+Q=="));
+                writer.write_event(Event::Empty(protection_elem))?;
             },
             Ok(Event::Eof) => break,
             // we can either move or borrow the event to write, depending on your use-case
